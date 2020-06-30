21b0180bf05a990831287da00824526aef519012
nanohana <fontana.osean.1412@gmail.com>
2020-06-30 18:30:29 +0900

    fix

--- mytask/simpleform/formapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from .models import Article
-# Create your models here.
-class PostModel(models.Model):
-    title = models.CharField(max_length=50)
-    memo = models.TextField()
